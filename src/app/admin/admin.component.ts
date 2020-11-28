876a03dbaaf98944729ab3c04ce2e1ca76daea08
Roy Allen Nidoy <allen.1120@icloud.com>
2020-11-29 05:35:21 +0800

    added sendMsg function

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { baseURL } from '../shared/baseurl';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
 })
 export class AdminComponent implements OnInit {
+  msg: string = '';
+  subscribers: any;
+  headers = { 'content-type': 'application/json' };
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {}
+  getVal = (item) => (this.msg = item.target.value);
+  sendMsg = () => {
+    this.http
+      .get(baseURL + '/dashboard/getAllCredentials')
+      .toPromise()
+      .then((data) => (this.subscribers = data))
+      .then((_) =>
+        this.subscribers.map((subscriber) => {
+          let { access_token, subscriber_number } = subscriber;
 
-  constructor() { }
+          var myHeaders = new Headers();
+          myHeaders.append('Content-Type', 'application/json');
 
-  ngOnInit(): void {
-  }
+          var raw = JSON.stringify({
+            outboundSMSMessageRequest: {
+              clientCorrelator: '123456',
+              senderAddress: '7354',
+              outboundSMSTextMessage: { message: this.msg },
+              address: `${subscriber_number}`,
+            },
+          });
 
+          var requestOptions: any = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow',
+          };
+
+          var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+            targetUrl =
+              'https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/7354/requests?access_token=' +
+              access_token;
+
+          fetch(proxyUrl + targetUrl, requestOptions)
+            .then((response) => response.text())
+            .then((result) => console.log(result))
+            .catch((error) => console.log('error', error));
+        })
+      );
+  };
 }
